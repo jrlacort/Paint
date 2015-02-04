4b3692adbca44b02442a50c2f949769e6c8c01ad
botas1 <d.botas@hotmail.com>
2015-02-04 11:30:22 +0100

    El primer commit del trabajo

--- src/codingbatstrings2/CatDogJaimeSotelo.java
+++ src/codingbatstrings2/CatDogJaimeSotelo.java
@@ -9,5 +9,16 @@ package codingbatstrings2;
  * @author xp
  */
 public class CatDogJaimeSotelo {
-    
+    public boolean catDog (String str) {
+    int count = 0;
+  for (int i=0; i<str.length(); i++) {
+    if (str.charAt(i) == 'e') count++;
+     }
+  if (count == 2){
+   return true;
+   }
+  return false;
+}
+  
+
 }
