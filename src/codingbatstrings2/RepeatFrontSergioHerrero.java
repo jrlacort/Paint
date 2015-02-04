27256a2c02b9ae40647528e8317be9f003c9cd38
Sergio <Sergio@Sergio-PC>
2015-02-04 14:30:50 +0100

    Iniciamos con esperanza aunque no tenga ni idea.

---
+++ src/codingbatstrings2/RepeatFrontSergioHerrero.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codingbatstrings2;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Sergio
+ */
+public class RepeatFrontSergioHerrero {
+    public String repeatFront(String str, int n) {
+        n = str.length();
+        String palabra = "";
+              
+        return palabra;
+        
+        
+}
+
+}
