46a539efec55dc14d36d6d3d49f9fa8d88e67a2a
Admin <Admin@MSI>
2015-02-04 14:25:26 +0100

    Repeat End - Jose Ramon Lacort

--- src/codingbatstrings2/RepeatEndJoseRamonLacort.java
+++ src/codingbatstrings2/RepeatEndJoseRamonLacort.java
@@ -10,5 +10,16 @@ package codingbatstrings2;
  * @author Admin
  */
 public class RepeatEndJoseRamonLacort {
-    
+public String repeatEnd(String str, int n) {
+String end;
+String resultado = "";
+ 
+end = str.substring(str.length() - n, str.length());
+ 
+for(int i = 0; i < n; i++) {
+resultado += end;
+}
+return resultado;
+}
+
 }
